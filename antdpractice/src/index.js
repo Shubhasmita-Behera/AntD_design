572d8712fc4202140bc0fd9ad911aab238000aca
Pronab <Pronab.pal@unimelb.edu.au>
2023-11-20 00:51:30 +1100

    fix cors and menulink

--- antdpractice/src/index.js
+++ antdpractice/src/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import App2 from './Loginfile';
+//import App2 from './Loginfile';
 
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-    <App />
-    <App2/>
-    </Router>
+   
+    <App/>
+    
   </React.StrictMode>
 );
 
